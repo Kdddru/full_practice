1070304df65de100c4d29a3ed02ce2bfe556039c
Kdddru <125441996+Kdddru@users.noreply.github.com>
2024-03-13 16:26:14 +0900

    package.json 수정밑 폴더 변경

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+
+const app = express();
+
+//build 안에 있는 파일들 허용
+app.use(express.static('build'));
+
+
+app.get('/',(req, res)=>{
+  res.sendFile(__dirname + '/build/index.html')
+})
+
+app.listen(3000,()=>{
+  console.log('server is running')
+})
